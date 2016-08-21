9eb584b42bc1263104c95b458c6932c3bc026c7c
Shreyansh <shreyanshgoel9@gmail.com>
2016-08-22 00:27:37 +0530

    small UI changes

--- monitoradblock/settings.php
+++ monitoradblock/settings.php
@@ -2,12 +2,12 @@
 <div class="wrap">
     <form action="" method="post" enctype="multipart/form-data">
         Your Message: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <input type="text" name="adblock_message" value="<?php echo $m;?>">
+        <input type="text" name="adblock_message" style="width:300px" value="<?php echo $m;?>">
         <br>
         <br> Image displayed: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <input type="file" name="adblock_image">
         <br>
         <br>
-        <input type="submit" name="save_image" value="upload">
+        <input type="submit" name="save_image" class="button-primary" value="upload">
     </form>
 </div>
